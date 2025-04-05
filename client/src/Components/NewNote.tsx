7c4845b7452c8632efc101666791e1ba1d4df0fe
Yash <yashkhot33@gmail.com>
2025-04-06 01:10:44 +0530

    fixed small error

--- client/src/Components/NewNote.tsx
+++ client/src/Components/NewNote.tsx
@@ -1,14 +1,15 @@
 import NotesForm from "./NoteForm";
+import {NoteData} from "../App"
 
 type NewNoteProps = {
-    onSubmit:(data:NoteData) => void
+    onSubmit:(data:NoteData) => void;
 };
 
 export function NewNote({onSubmit}:NewNoteProps){
     return (
         <>
         <h1>New Note</h1>
-        <NotesForm/>`
+        <NotesForm onSubmit={onSubmit}/>
         </>
     )
 }
